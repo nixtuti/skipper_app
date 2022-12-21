eb1098d80190e28d3fd7368475aaed07d2e3eeb7
EC2 Default User <111040404+nixtuti@users.noreply.github.com>
2022-12-21 09:50:03 +0000

    [Add]javascriptを学ぼう 完了

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,23 @@
 const { environment } = require('@rails/webpacker')
 
+// const webpack = require('webpack')
+// environment.plugins.prepend(
+//   'Provide',
+//   new webpack.ProvidePlugin({
+//     $: 'jquery/src/jquery',
+//     jQuery: 'jquery/src/jquery',
+//       "window.jQuery": "jquery"
+//   })
+// )
+
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    "window.jQuery": "jquery"  })
+)
+
+
 module.exports = environment
